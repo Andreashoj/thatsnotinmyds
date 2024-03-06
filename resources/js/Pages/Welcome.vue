be3301e62258e1dbe30d3b606a68ec5e493afec3
Andreas Høj <andrewhoj@gmail.com>
2024-03-06 09:37:30 +0100

    chore: init

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <main>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+
+        <header>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            hey there!
+
+            <button class="text-blue-500" @click="count += 1">adder</button>
+            {{ count }}
+        </header>
+        <article>
+            <slot />
+        </article>
+    </main>
+</template>

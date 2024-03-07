9686007e40beca284b66407348b687d427fb0ab3
Andreas Høj <andrewhoj@gmail.com>
2024-03-07 13:55:59 +0100

    chore: test

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,7 +19,8 @@ const count = ref(0)
 
             can i update??
             YOO
-THIS IS SICK!
+            YOOOOOOOOO
+            THIS IS SICK!
             <button class="text-blue-500" @click="count += 1">adder</button>
             {{ count }}
         </header>

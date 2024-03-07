9a0b9fe43dc7c18f7ac37037f50355b4ef619af0
Andreas Høj <andrewhoj@gmail.com>
2024-03-07 12:34:45 +0100

    chchore: remove url thingy

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost').':'.env('APP_PORT', '80'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 

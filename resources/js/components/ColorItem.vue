67c21ebcb546aa7c69a3175462af563abed736aa
Andreas Høj <andrewhoj@gmail.com>
2024-03-11 16:16:15 +0100

    chore: optimizations

--- resources/js/components/ColorItem.vue
+++ resources/js/components/ColorItem.vue
@@ -20,7 +20,7 @@ const getTextColor = computed(() => {
         :class="`bg-${color}-${shade}`" :style="`color: ${getTextColor}`">
         <div class="absolute top-0 left-0 w-full h-6 text-left flex items-center">
             <div class="h-full w-full bg-black opacity-60 absolute z-0" />
-            <span class="z-10 text-gray-300 relative px-2">
+            <span class="z-10 text-gray-200 relative px-2">
                 #{{ index }}
             </span>
         </div>
